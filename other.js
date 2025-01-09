d8ddc73b5b04c90089c35ccb23d032522175df6a
BeeSwarmHacks <imbluedabadee1@proton.me>
2025-01-09 14:24:10 -0500

    Update other.js

--- other.js
+++ other.js
@@ -1974,7 +1974,7 @@ function BeeSwarmSimulator(DATA){
             }
         },
 
-        enterTunnel:{
+        enter:{
 
             isMachine:true,minX:-69.8-1,maxX:-69.8+1,minY:17,maxY:20,minZ:59-1,maxZ:59+1,message:'Enter Mysterious Hole',func:function(player){
                 
@@ -21975,7 +21975,7 @@ function BeeSwarmSimulator(DATA){
         gummyBaller:{
             
             collectPattern:[[-3,-3],[-2,-5],[-2,-4],[-2,-3],[-2,-2],[-2,-1],[-1,-5],[-1,-4],[-1,-3],[-1,-2],[-1,-1],[0,-6],[0,-5],[0,-4],[0,-3],[0,-2],[0,-1],[0,0],[1,-5],[1,-4],[1,-3],[1,-2],[1,-1],[2,-5],[2,-4],[2,-3],[2,-2],[2,-1],[3,-3]],
-            collectAmount:16,
+            collectAmount:9999,
             cooldown:1,
             mesh:function(box,cylinder,sphere,star){
                 
